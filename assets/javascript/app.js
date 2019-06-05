f6c6120c8f349248ecb7c2e0a786354d80e4c959
Molly Graham <mollyclaire.graham@gmail.com>
2019-06-05 17:17:44 -0500

    Added code for answer buttons to appear on the page and revised and reorganized my pseudocode.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,27 +5,27 @@ var questionList = [
     {
         question: "What art form was born because of the collapse of social and moral values during WWI?",
         answers: ["Cubism", "Etruscan Art", "Pop Art", "Dada"],
-        correctAnswer: 3
+        correctAnswer: "Dada"
     },
     {
         question: "Scenes from an American's everyday life were shown through which of these art movements?",
         answers: ["Abstract Expressionism", "Modernism", "Impressionism", "American Scene Painting"],
-        correctAnswer: 3
+        correctAnswer: "American Scene Painting"
     },
     {
         question: "What style of painting leaves a very thick layer of paint on the canvas?",
         answers: ["Impasto", "Pointillism", "Hatching", "Sfumato"],
-        correctAnswer: 0
+        correctAnswer: "Impasto"
     },
     {
         question: "What style of painting most emphasizes an immediate visual image of a scene?",
         answers: ["Expressionism", "Impressionism", "Photorealism", "Idealism"],
-        correctAnswer: 1
+        correctAnswer: "Impressionism"
     },
     {
         question: "Which art movement emphasizes the expression of imagination, free of the restraints of reason?",
         answers: ["Realism", "Expressionism", "Surrealism", "Constructivism"],
-        correctAnswer: 2
+        correctAnswer: "Surrealism"
     }
 ];
 
@@ -34,47 +34,96 @@ var correctAnswer;
 var correct = 0;
 var incorrect = 0;
 var unanswered; 
-
-// User clicks "Start" button to begin
-var startButton = $('<input/>').attr({ type: 'button', name:'btn1', value:'a button' });
-$("#start").append(startButton);
-startButton.text("Start");
-
-// Function to display questions and answer choices
-// var currentquestion = 0;
-// function displayQuestion() {
-//     $("#question").text(questionList[currentquestion].question);
-//     var options = questionList[currentquestion].answers;
-//     console.log(options);
-//     for (var i = 0; i < options.length; i++) {
-//     console.log(questionList[currentquestion].answers[i]);
-//   }
-
-//     }
 var currentQuestion = 0;
-function displayQuestion() {
-    let q = questionList[currentQuestion].question;
+var sec = 30;
+var s = 5;
 
-        console.log(q);
-    let a = questionList[currentQuestion].answers;
-    
-        $("#question").text(q);
-        $("#answers").text(a);
 
-    }
-    displayQuestion();
 
 // Create function for a 30 sec timer that applies to only one question
+var timerThirty = setInterval(function() {
+    $("#quiz").text(sec--);
+    if (sec == -1) {
+        clearInterval(timerThirty);
+    }
+    }, 30000);
 
-var sec = 30
-var timer = setInterval(function() {
-   $('#timer').text(sec--);
-   if (sec == -1) {
-      clearInterval(timer);
-   }
-}, 1000);
+// Create function for a 5 sec timer that applies to the "score page"
+var timerFive = setInterval(function() {
+    $("#quiz").text(s--);
+    if (s == -1) {
+        clearInterval(timerFive);
+    }
+    }, 1000);
 
-// User clicks on an answer
+// $("#start-button").on("click", function() {
+//     displayQuestion();
+//     // timerThirty();
+//   })
+
+  $(document).on("click", "#start-button", function() {
+    displayQuestion();
+  })
+
+// Display question and answer choices function
+function displayQuestion() {
+    
+    let q = questionList[currentQuestion].question;
+    // for (var i = 0; i < q.length; i++) {
+    //     console.log(q);
+    // }
+    $("#quiz").html("<h1>" + q + "</h1>");
+
+       
+    let a = questionList[currentQuestion].answers;
+    for (var i = 0; i < a.length; i++) {
+        console.log(a);
+        // $("#answers").text(a);
+        $("#quiz").append("<button class='answer-button' id='button' data-name='" + questionList[currentQuestion].answers[i]
+        + "'>" + questionList[currentQuestion].answers[i] + "</button>");
+    }
+    
+        $("#question").text(q);
+        timerThirty();
+        
+
+    }
+
+
+// What happens when a user clicks on an answer (30 sec timer stops, if the user selects the right answer or wrong answer)
+var clicked = function(e) {
+    clearInterval(timer);
+    if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer) {
+     answerCorrectly();
+    }
+    else {
+      answerIncorrectly();
+    }
+  }
+
+// The next question is displayed and the timer resets
+function nextQuestion() {
+    this.currentQuestion++
+    displayQuestion();
+    timerFive();
+}
+nextQuestion();
+
+// I still need to create the conditional for answerCorrectly() function {}
+
+// I still need to create the conditionals for answerIncorrectly() function{}
+
+// Click functions
+// Answer button
+$(document).on("click", ".answer-button", function(e) {
+    clicked(e);
+  })
+// Start button 
+// $(document).on("click", "#start-button", function() {
+//     displayQuestion();
+//   })
+
+
     // If it's wrong...
         // The text where the question and answers is replaced with "Incorrect!," the correct answer, and an image
         // After a few seconds, the next question appears
@@ -89,12 +138,5 @@ var timer = setInterval(function() {
     // Reset timer
 // After all questions have been presented, display the user's score and a "Start Over" button
 
-
-
-// for(var i = 0; i < questionList.length; i++){
-//     var displayQuestion = questionList[i].question};
-
-// console.log(displayQuestion);
-// console.log(options);
 
 });
