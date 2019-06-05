5307843f8eff4530d85360f6f6e682b5224e5545
Molly Graham <mollyclaire.graham@gmail.com>
2019-06-04 19:43:20 -0500

    Attempted to get my questions and answers to appear in the browser.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,70 @@
-// User clicks "Start" button to begin
-// Create object with question and answers
-var questions = [
-    {question: "1. What art form was born because of the collapse of social and moral values during WWI?",
-    choices: ["Cubism", "Etruscan Art", "Pop Art", "Dada"],
-    correctAnswer: 3},
-    {question: "2. Scenes from an American's everyday life were shown through which of these art movements?",
-    choices: ["Abstract Expressionism", "Modernism", "Impressionism", "American Scene Painting"],
-    correctAnswer: 3},
-    {question: "3. What style of painting leaves a very thick layer of paint on the canvas?",
-    choices: ["Impasto", "Pointillism", "Hatching", "Sfumato"],
-    correctAnswer: 0},
-    {question: "4. What style of painting most emphasizes an immediate visual image of a scene?",
-    choices: ["Expressionism", "Impressionism", "Photorealism", "Idealism"],
-    correctAnswer: 1},
-    {question: "5. Which art movement emphasizes the expression of imagination, free of the restraints of reason?",
-    choices: ["Realism", "Expressionism", "Surrealism", "Constructivism"],
-    correctAnswer: 2}
-]
+$(document).ready(function() {
 
-// Put first question and answers on the page with jQuery
+// Create object with question and answers
+var questionList = [
+    {
+        question: "What art form was born because of the collapse of social and moral values during WWI?",
+        answers: ["Cubism", "Etruscan Art", "Pop Art", "Dada"],
+        correctAnswer: 3
+    },
+    {
+        question: "Scenes from an American's everyday life were shown through which of these art movements?",
+        answers: ["Abstract Expressionism", "Modernism", "Impressionism", "American Scene Painting"],
+        correctAnswer: 3
+    },
+    {
+        question: "What style of painting leaves a very thick layer of paint on the canvas?",
+        answers: ["Impasto", "Pointillism", "Hatching", "Sfumato"],
+        correctAnswer: 0
+    },
+    {
+        question: "What style of painting most emphasizes an immediate visual image of a scene?",
+        answers: ["Expressionism", "Impressionism", "Photorealism", "Idealism"],
+        correctAnswer: 1
+    },
+    {
+        question: "Which art movement emphasizes the expression of imagination, free of the restraints of reason?",
+        answers: ["Realism", "Expressionism", "Surrealism", "Constructivism"],
+        correctAnswer: 2
+    }
+];
+
+// User clicks "Start" button to begin
+
+// Function to display questions and answer choices
+var currentquestion = 0;
+function displayQuestion() {
+    $("#question").text(questionList[currentquestion].question);
+    var options = questionList[currentquestion].choices;
+    console.log(options);
+    for (var i = 0; i < options.length; i++) {
+    console.log(questionList[currentquestion].choices[i]);
+  }
+
+    }
+
 // Include timer that applies to only one question
 // User clicks on an answer
     // If it's wrong...
         // The text where the question and answers is replaced with "Incorrect!," the correct answer, and an image
         // After a few seconds, the next question appears
+        // Reset timer
     // If it's correct...
         // The text where the question and answers is replaced with "Correct!" and an image
+        // The timer is stopped 
+        // After a few seconds, the next question appears
+        // Reset timer
 // If the user does NOT click on an answer and the timer runs out
     // The next question is displayed
+    // Reset timer
 // After all questions have been presented, display the user's score and a "Start Over" button
+
+
+
+// for(var i = 0; i < questionList.length; i++){
+//     var displayQuestion = questionList[i].question};
+
+// console.log(displayQuestion);
+// console.log(options);
+
+});
